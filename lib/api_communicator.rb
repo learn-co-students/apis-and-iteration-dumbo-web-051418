ad12bb2f529dbde9274a603fb16eafa16da02195
pablopawpaw <lpault29@gmail.com>
2018-05-15 18:33:21 -0400

    add methods to get char movies and use it to grab approp apis data to then filter through for wanted info

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -19,21 +19,21 @@ def get_character_movies_from_api(character)
   character_films
 end
 
-def get_movie_info_from_api
-  all_movies_for_character = RestClient.get('http://www.swapi.co/api/films/')
-  parsed_movies = JSON.parse(all_movies_for_character)
-end
-
-def get_movies_info(movies_hash)
-  movie_list = []
-  movies_hash["results"].map do |element|
-    movie_list << element
-  end
-  movie_list
-end
-
 #get_character_movies_from_api("Luke Skywalker")
-get_movies_info(get_movie_info_from_api)
+
+def get_char_movie_info_from_api(character)
+  char_urls = get_character_movies_from_api(character)
+  clean_movie_array = []
+  char_urls.each do |x|
+    all_movies_for_character = RestClient.get(x)
+    parsed_movies = JSON.parse(all_movies_for_character)
+    clean_movie_array << parsed_movies
+  end
+   clean_movie_array
+end
+
+
+#get_char_movie_info_from_api("Luke Skywalker")
 
   # this collection will be the argument given to `parse_character_movies`
   #  and that method will do some nice presentation stuff: puts out a list
@@ -42,8 +42,14 @@ get_movies_info(get_movie_info_from_api)
 
 def parse_character_movies(movie_list)
   # some iteration magic and puts out the movies in a nice list
-
+  movie_by_title = []
+  movie_list.map do |x|
+  movie_by_title << x["title"]
+  end
+  puts movie_by_title
 end
+
+parse_character_movies(get_char_movie_info_from_api("Luke Skywalker"))
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)

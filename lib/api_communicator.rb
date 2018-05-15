12c38864cc0db335b698993d57b56f2072662fdb
pablopawpaw <lpault29@gmail.com>
2018-05-15 19:29:12 -0400

    cleaned up and added comments

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,12 +3,13 @@ require 'json'
 require 'pry'
 
 def get_info_from_api
+  #grab all data from api to use
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
 end
 
 def get_character_movies_from_api(character)
-  #make the web request
+  #grabbing char movie urls
   character_films = nil
   get_info_from_api["results"].map do |element|
     char_name = element["name"]
@@ -18,10 +19,10 @@ def get_character_movies_from_api(character)
   end
   character_films
 end
-
 #get_character_movies_from_api("Luke Skywalker")
 
 def get_char_movie_info_from_api(character)
+  #grab all movie info from char data list into an array
   char_urls = get_character_movies_from_api(character)
   clean_movie_array = []
   char_urls.each do |x|
@@ -31,30 +32,23 @@ def get_char_movie_info_from_api(character)
   end
    clean_movie_array
 end
-
-
 #get_char_movie_info_from_api("Luke Skywalker")
 
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
-
-
 def parse_character_movies(movie_list)
-  # some iteration magic and puts out the movies in a nice list
+  # some iteration magic and returns the movies in a nice list
   movie_by_title = []
   movie_list.map do |x|
-  movie_by_title << x["title"]
+    movie_by_title << x["title"]
   end
-  puts movie_by_title
+  movie_by_title
 end
-
-parse_character_movies(get_char_movie_info_from_api("Luke Skywalker"))
+#puts parse_character_movies(get_char_movie_info_from_api("Luke Skywalker"))
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
+  films_hash = get_char_movie_info_from_api(character)
   parse_character_movies(films_hash)
 end
+#puts show_character_movies("Luke Skywalker")
 
 ## BONUS
 

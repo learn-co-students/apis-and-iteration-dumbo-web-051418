889333665883641ad3135d18730adeabffa123fe
mrstevenaweiss <weiss.steven@gmail.com>
2018-05-15 19:42:21 -0400

    Steven/Malorie Apis

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -3,31 +3,49 @@ require 'json'
 require 'pry'
 
 def get_character_movies_from_api(character)
-  #make the web request
+  # Takes a SW's character, returns array of hashes of character's movies
   all_characters = RestClient.get('http://www.swapi.co/api/people/')
   character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+
+  # Making a list of all the film URL's
+  array = []
+  character_hash["results"].each do |data|
+    if data["name"] == character
+      array << data['films']
+    end
+  end
+  get_movie_data(array.flatten)
 end
 
-def parse_character_movies(films_hash)
-  # some iteration magic and puts out the movies in a nice list
+
+def get_movie_data(array)
+  # Helper function: "get_character_movies_from_api"
+  # Takes array of URL's, returns array of hashes for character argument
+  data = array.collect do |link|
+    all_info = RestClient.get(link)
+    movie_hash = JSON.parse(all_info)
+  end
+  data
 end
+
+
+def parse_character_movies(films_array)
+  # Enumerating through the array passed from 'get_character_movies_from_api', returning values of film title key
+  films_array.collect do |film|
+      film["title"]
+    end
+end
+
 
 def show_character_movies(character)
-  films_hash = get_character_movies_from_api(character)
-  parse_character_movies(films_hash)
+  # Main function.  Returning movie titles of argument/character.
+  films_array = get_character_movies_from_api(character)
+  parse_character_movies(films_array)
 end
 
 ## BONUS
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+#Pry.start
